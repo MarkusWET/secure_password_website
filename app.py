3223c4cab9c06aec1c1d3daeb631a7f5e916408c
Markus Wetzlmayr <ic17m014@technikum-wien.at>
2017-10-07 17:23:50 +0200

    enable debug mode, create login page

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = request.form['username']
+    password = request.form['password']
+    return "<p>{} - {}</p>".format(user, password)
 
 
 if __name__ == '__main__':
