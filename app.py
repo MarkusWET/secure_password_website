c5f134b521af6b1fb80e615eb1a57ac3373f8033
Markus Wetzlmayr <ic17m014@technikum-wien.at>
2017-10-08 13:12:43 +0200

    pbkdf2 variant added, secret page extended

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID as pgUUID, TEXT
 import uuid
+import os
 import hashlib
+import base64
 
 app = Flask(__name__)
 app.debug = True
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dbadmin@localhost:5432/secure_password_storage"
+HASH_ALGORITHM = "sha256"
+ITERATIONS = 100000
 db = SQLAlchemy(app)
 
 
@@ -25,12 +28,13 @@ class Users(db.Model):
     iterations = db.Column("iterations", db.Integer)
 
     # Methods
-    def __init__(self, username, password, password_md5, password_pbkdf2, salt):
+    def __init__(self, username, password, password_md5, password_pbkdf2, salt, iterations):
         self.username = username
         self.password = password
         self.password_md5 = password_md5
         self.password_pbkdf2 = password_pbkdf2
         self.salt = salt
+        self.iterations = iterations
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -43,6 +47,14 @@ def digest_password_md5(password):
     return digester.hexdigest()
 
 
+def digest_password_pbkdf2(password, algo, salt, iterations):
+    derived_key = hashlib.pbkdf2_hmac(hash_name=algo,
+                                      password=str.encode(password),
+                                      salt=salt,
+                                      iterations=iterations)
+    return base64.b64encode(derived_key)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -52,10 +64,23 @@ def index():
 def login():
     user = Users.query.filter_by(username=request.form["username"]).first()
 
-    if user is not None and user.password_md5 == digest_password_md5(request.form["password"]):
-        return render_template("secret_page.html")
-    else:
-        return render_template("index.html", error_text="User and password do not match.")
+    # Plaintext login
+    # if user is not None and user.password == request.form["password"]:
+
+    # MD5 login
+    # if user is not None and user.password_md5 == digest_password_md5(request.form["password"]):
+
+    # PBKDF2 login
+    if user is not None:
+        password = request.form["password"]
+        salt = user.salt
+        salt_b64_bytes = base64.b64decode(salt)
+        iterations = user.iterations
+        password_challenge = digest_password_pbkdf2(password, HASH_ALGORITHM, salt_b64_bytes, iterations)
+        if user.password_pbkdf2.encode() == password_challenge:  # digest_password_pbkdf2(request.form["password"], HASH_ALGORITHM, base64.b64decode(user.salt), ITERATIONS):
+            return render_template("secret_page.html")
+
+    return render_template("index.html", error_text="User and password do not match.")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -68,8 +93,23 @@ def register():
     password_confirmation = request.form["password_confirmation"]
 
     if password == password_confirmation:
-        password_digest = digest_password_md5(password)
-        user = Users(username, None, password_digest, None, None)
+        # Plaintext
+        # user = Users(username, password, None, None, None)
+
+        # MD5 hashed password
+        # password_digest = digest_password_md5(password)
+        # user = Users(username, None, password_digest, None, None)
+
+        # PBKDF2 secure hashed and salted password
+        salt = os.urandom(16)
+        salt_b64 = base64.b64encode(salt).decode()  # salt bytes --> b64 bytes --> string
+
+        password_digest_bytes = digest_password_pbkdf2(password,
+                                                       HASH_ALGORITHM,
+                                                       salt,
+                                                       ITERATIONS)
+        digest_string = password_digest_bytes.decode()
+        user = Users(username, None, None, digest_string, salt_b64, ITERATIONS)
         db.session.add(user)
         db.session.commit()
 
